cfde15ff9f6a6ae7d5aea18f3751f78f2dfeaf5e
Chris Kumor <134901634+ziomtechstudios@users.noreply.github.com>
2023-12-08 17:16:40 -0500

    Attempting to use tile locations to further define sprite layering of player
    Ultimate goal, detect what tile player is standing on when colliding with underwall collider.
    If player is standing on building floor tile, we know that the player is inside building and when up against the wall should be layered above the wall.
    If the player is standing on an environmental tile then the player is outside and should be layered behind the wall.
    The particular code i added is not providing desired outcome so i will have to use other functions to figure this out.

--- Assets/Scripts/DynamicSpriteLayering.cs
+++ Assets/Scripts/DynamicSpriteLayering.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 namespace Com.ZiomtechStudios.ForgeExchange{
 [RequireComponent(typeof(SpriteRenderer))]
@@ -9,17 +10,21 @@ public class DynamicSpriteLayering : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer m_SpriteRend;
     [SerializeField] private BoxCollider2D m_BoxCollider;
+    [SerializeField] private Tilemap buildingTileMap;
     // Start is called before the first frame update
     void Start()
     {
         m_SpriteRend = GetComponent<SpriteRenderer>();
         m_BoxCollider = GetComponent<BoxCollider2D>();
+        buildingTileMap = GameObject.Find("Grid").transform.Find("tilemap_buildings").gameObject.GetComponent<Tilemap>();
     }
 
     // Called when attached gameObjects collider triggers another gameObject's collider
     void OnTriggerEnter2D(Collider2D col)
-    {
-        if(col.CompareTag("underwall"))
+    {         
+        Debug.Log(buildingTileMap.GetTile(Vector3Int.FloorToInt(transform.position)) != null);
+        //Are we colliding with a building wall?  //Are we currently in a room or outside of it?
+        if(col.CompareTag("underwall") && (buildingTileMap.GetTile(Vector3Int.FloorToInt(transform.position)) != null))
             m_SpriteRend.sortingOrder = 0;
     }
     void OnTriggerExit2D(Collider2D col){
