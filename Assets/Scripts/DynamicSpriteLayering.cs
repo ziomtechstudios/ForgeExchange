c9fd5b7b11c4f9c8ceeb132ccca05984c3f7505d
Chris Kumor <134901634+ziomtechstudios@users.noreply.github.com>
2023-12-12 11:18:08 -0500

    Adding another layer for dynamicsprtelayering script
    I did this so that the player has the ability to walk behind banners when they are inside a building. I am adding this for a potential stealth mechanism o for when players have to look for hidden items.

--- Assets/Scripts/DynamicSpriteLayering.cs
+++ Assets/Scripts/DynamicSpriteLayering.cs
@@ -15,16 +15,30 @@ namespace Com.ZiomtechStudios.ForgeExchange{
         [SerializeField] private Tilemap buildingTileMap;
         [SerializeField] private Tilemap environmentTileMap;
         [SerializeField] private Tilemap underwallTileMap;
+        [SerializeField] private Tilemap ornamentsTileMap;
         [SerializeField] private Transform m_SpriteSortPoint;
+        [SerializeField] private Transform m_OrnamentSortPoint;
         #endregion
         #region "Private memebers"
         private GridLayout m_GridLayout;
+        private void FadingWall(){
+
+        }
         private void SortTargetSprite(Collider2D col){
-            //Are we colliding with a building wall?  //Is the tile the player is currently on a tile correlating to the buildings or tapestries?
-            if(col.CompareTag("underwall") && (buildingTileMap.GetTile(m_GridLayout.WorldToCell(m_SpriteSortPoint.position)) != null))
-                m_SpriteRend.sortingOrder = 1;
-            else if(col.CompareTag("underwall") && ((environmentTileMap.GetTile(m_GridLayout.WorldToCell(m_SpriteSortPoint.position)) != null) || (underwallTileMap.GetTile(m_GridLayout.WorldToCell(m_SpriteSortPoint.position)) != null)))
-                m_SpriteRend.sortingOrder = 0;
+            //Are we colliding with a building wall?  
+            if(col.CompareTag("underwall")){
+                //Players feet is on a floor tile so we are in from of the wall
+                if(buildingTileMap.GetTile(m_GridLayout.WorldToCell(m_SpriteSortPoint.position)) != null && ornamentsTileMap.GetTile(m_GridLayout.WorldToCell(m_OrnamentSortPoint.position)) == null) 
+                    m_SpriteRend.sortingOrder = 2;                
+                //The p[ayer is outside and behind a wall
+                else if((environmentTileMap.GetTile(m_GridLayout.WorldToCell(m_SpriteSortPoint.position)) != null) && (underwallTileMap.GetTile(m_GridLayout.WorldToCell(m_SpriteSortPoint.position)) != null) && (buildingTileMap.GetTile(m_GridLayout.WorldToCell(m_SpriteSortPoint.position)) == null))
+                    m_SpriteRend.sortingOrder = 0;
+                //we are standing behind something hanging on the walls
+                else if(ornamentsTileMap.GetTile(m_GridLayout.WorldToCell(m_OrnamentSortPoint.position)) != null)
+                    m_SpriteRend.sortingOrder = 1;
+                else
+                    m_SpriteRend.sortingOrder = 2;
+            }
         }
         #endregion
         // Start is called before the first frame update
@@ -36,7 +50,9 @@ namespace Com.ZiomtechStudios.ForgeExchange{
             buildingTileMap = m_GridLayout.gameObject.transform.Find("tilemap_buildings").gameObject.GetComponent<Tilemap>();
             environmentTileMap = m_GridLayout.gameObject.transform.Find("tilemap_environment").gameObject.GetComponent<Tilemap>();
             underwallTileMap = m_GridLayout.gameObject.transform.Find("tilemap_underwall").gameObject.GetComponent<Tilemap>();
+            ornamentsTileMap  = m_GridLayout.gameObject.transform.Find("tilemap_underwall/tilemap_ornaments").gameObject.GetComponent<Tilemap>();
             m_SpriteSortPoint = transform.Find("SpriteSortPos");
+            m_OrnamentSortPoint = transform.Find("ornamentSortPoint");
         }
 
         // Called when attached gameObjects collider triggers another gameObject's collider
