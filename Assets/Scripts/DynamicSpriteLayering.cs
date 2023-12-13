df4b2836c5a43df0668405181114a6bc027e5731
Chris Kumor <134901634+ziomtechstudios@users.noreply.github.com>
2023-12-13 11:27:10 -0500

    Adding transparency to building walls when behind them.

--- Assets/Scripts/DynamicSpriteLayering.cs
+++ Assets/Scripts/DynamicSpriteLayering.cs
@@ -9,7 +9,7 @@ namespace Com.ZiomtechStudios.ForgeExchange{
     [RequireComponent(typeof(BoxCollider2D))]
     public class DynamicSpriteLayering : MonoBehaviour
     {
-        #region "rivate serialized Fields"
+        #region "Private serialized Fields"
         [SerializeField] private SpriteRenderer m_SpriteRend;
         [SerializeField] private BoxCollider2D m_BoxCollider;
         [SerializeField] private Tilemap buildingTileMap;
@@ -18,6 +18,7 @@ namespace Com.ZiomtechStudios.ForgeExchange{
         [SerializeField] private Tilemap ornamentsTileMap;
         [SerializeField] private Transform m_SpriteSortPoint;
         [SerializeField] private Transform m_OrnamentSortPoint;
+        [Tooltip("WHen passing a value for transparancy workwithin the range of 0.0f to 255.0f. Take your target value and divide it by 255.0f so that it will result in a nuumber between 0.0f and 1.0f.")][Range(0.0f, 1.0f)] [SerializeField] private float transparancyVal;
         #endregion
         #region "Private memebers"
         private GridLayout m_GridLayout;
@@ -28,16 +29,21 @@ namespace Com.ZiomtechStudios.ForgeExchange{
             //Are we colliding with a building wall?  
             if(col.CompareTag("underwall")){
                 //Players feet is on a floor tile so we are in from of the wall
-                if(buildingTileMap.GetTile(m_GridLayout.WorldToCell(m_SpriteSortPoint.position)) != null && ornamentsTileMap.GetTile(m_GridLayout.WorldToCell(m_OrnamentSortPoint.position)) == null) 
-                    m_SpriteRend.sortingOrder = 2;                
+                if(buildingTileMap.GetTile(m_GridLayout.WorldToCell(m_SpriteSortPoint.position)) != null && ornamentsTileMap.GetTile(m_GridLayout.WorldToCell(m_OrnamentSortPoint.position)) == null){
+                    m_SpriteRend.sortingOrder = 2;
+                    underwallTileMap.color = Color.Lerp(new Color(1.0f, 1.0f, 1.0f, (transparancyVal/255.0f)), Color.white, 1.00f);
+                }                
                 //The p[ayer is outside and behind a wall
-                else if((environmentTileMap.GetTile(m_GridLayout.WorldToCell(m_SpriteSortPoint.position)) != null) && (underwallTileMap.GetTile(m_GridLayout.WorldToCell(m_SpriteSortPoint.position)) != null) && (buildingTileMap.GetTile(m_GridLayout.WorldToCell(m_SpriteSortPoint.position)) == null))
+                else if(((environmentTileMap.GetTile(m_GridLayout.WorldToCell(transform.position)) != null) || (underwallTileMap.GetTile(m_GridLayout.WorldToCell(m_SpriteSortPoint.position)) != null)) && (buildingTileMap.GetTile(m_GridLayout.WorldToCell(m_SpriteSortPoint.position)) == null)){
                     m_SpriteRend.sortingOrder = 0;
+                    underwallTileMap.color = Color.Lerp(Color.white, new Color(1.0f, 1.0f, 1.0f, transparancyVal), 1.00f);
+                }
                 //we are standing behind something hanging on the walls
                 else if(ornamentsTileMap.GetTile(m_GridLayout.WorldToCell(m_OrnamentSortPoint.position)) != null)
                     m_SpriteRend.sortingOrder = 1;
                 else
                     m_SpriteRend.sortingOrder = 2;
+                    //
             }
         }
         #endregion
@@ -64,6 +70,7 @@ namespace Com.ZiomtechStudios.ForgeExchange{
         }
         void OnTriggerExit2D(Collider2D col){
             SortTargetSprite(col);
+            underwallTileMap.color = Color.Lerp(new Color(1.0f, 1.0f, 1.0f, (transparancyVal/255.0f)), Color.white, 1.00f);
         }
     }
 }
