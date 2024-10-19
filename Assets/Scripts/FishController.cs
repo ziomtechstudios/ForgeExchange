3f54415b85595eeb472ddae36552a1f5cc4f664a
ziomtechstudios <134901634+ziomtechstudios@users.noreply.github.com>
2024-10-19 15:41:41 -0400

    Debugging Adamant Sword Attack bug,  and conceptualizing in-game fish.

---
+++ Assets/Scripts/FishController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.ZiomtechStudios.ForgeExchange{
+    public class FishController : MonoBehaviour
+    {
+        #region "Private Seiralized Fields"
+        
+        #endregion
+    }
+}
