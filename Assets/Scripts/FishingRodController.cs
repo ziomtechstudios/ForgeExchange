0cb2a222f22ea05d7e60c7b16d5b961e0d696f30
ziomtechstudios <134901634+ziomtechstudios@users.noreply.github.com>
2025-10-03 14:14:13 -0400

    Debug inconsistent triggering of  collision with fish spawner.
    Gave fishing Rod static shape for its polygon 2D collider so that when the player casts there rod near the fish spawner it will consistently trigger future code to determine if and what kind of fish will take the bait and begin to fishing mini-game.

--- Assets/Scripts/FishingRodController.cs
+++ Assets/Scripts/FishingRodController.cs
@@ -1,25 +1,22 @@
-
-using System;
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
-
 namespace Com.ZiomtechStudios.ForgeExchange{
     public class FishingRodController : ItemController
     {
+        #region "Private Fields"
         [SerializeField] private Animator m_Animator;
         [SerializeField] private float curReeledAmnt;
         [SerializeField] private float maxReelAmnt;
         // Start is called before the first frame update
         private int isRodCastingHash, isRodReelingHash, isRodFullyReeledHash, lookDirXHash, lookDirYHash;
-
-
+        #endregion
+        #region "Public Getters/Setters"
         public int IsRodReelingHash { get { return isRodReelingHash;} set { isRodReelingHash = value;}}
         public int IsRodCastingHash { get { return isRodCastingHash;} set { isRodCastingHash =value;}}
         public int IsRodFullyReeledHash {get { return isRodFullyReeledHash; } set { isRodFullyReeledHash = value;}}
         public float MaxReelAmnt { get { return maxReelAmnt;}}
         public float CurReeledAmnt{ get { return curReeledAmnt;} set{curReeledAmnt = value;}}
         public Animator M_Animator{set{m_Animator = value;}get{return m_Animator;}}
-
+        #endregion
         public void CastRod(PlayerController playerCont){
             m_Animator.SetTrigger(isRodCastingHash);
             m_Animator.SetBool(isRodReelingHash, false);
@@ -30,7 +27,6 @@ namespace Com.ZiomtechStudios.ForgeExchange{
         public void RodReeling(bool isReeling){
             m_Animator.SetBool(IsRodReelingHash, isReeling);
         }
-        
         void Awake()
         {
             m_Animator = GetComponent<Animator>();
@@ -40,8 +36,7 @@ namespace Com.ZiomtechStudios.ForgeExchange{
             lookDirXHash = Animator.StringToHash("LookX");
             lookDirYHash = Animator.StringToHash("LookY");
         }
-
-        public void OnCollisionEnter2D(Collision2D other)
+        void OnCollisionEnter2D(Collision2D other)
         {
             if(other.gameObject.CompareTag("Spawner") && LayerMask.LayerToName(other.gameObject.layer) == "Water")
                 Debug.Log("The player's fishing line is contacting a fish spawner.");
