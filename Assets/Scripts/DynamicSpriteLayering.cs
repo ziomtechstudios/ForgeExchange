bf2a7d17f67b0fe61c65b5ac6a72afdc6b87b9c5
ziomtechstudios <134901634+ziomtechstudios@users.noreply.github.com>
2025-11-08 16:02:39 -0500

    Debugging Sprite Layering Script

--- Assets/Scripts/DynamicSpriteLayering.cs
+++ Assets/Scripts/DynamicSpriteLayering.cs
@@ -13,7 +13,7 @@ namespace Com.ZiomtechStudios.ForgeExchange{
         [SerializeField] private Tilemap underwallTileMap;
         [SerializeField] private Tilemap ornamentsTileMap;
         [SerializeField] private Tilemap waterTileMap;
-        [SerializeField] private Tilemap entryExitTilemap;
+        [SerializeField] private Tilemap entryExitTileMap;
         [SerializeField] private Transform m_SpriteSortPoint;
         [SerializeField] private Transform m_OrnamentSortPoint;
         [Tooltip("When passing a value for transparancy work within the range of 0.0f to 255.0f. Take your target value and divide it by 255.0f so that it will result in a nuumber between 0.0f and 1.0f.")][Range(0.0f, 1.0f)] [SerializeField] private float transparancyVal;
@@ -29,20 +29,20 @@ namespace Com.ZiomtechStudios.ForgeExchange{
             //Are we colliding with a building wall?  
             if(col.CompareTag("underwall")){
                 isInside = (buildingTileMap.GetTile(m_GridLayout.WorldToCell(m_SpriteSortPoint.position)) != null && ornamentsTileMap.GetTile(m_GridLayout.WorldToCell(m_OrnamentSortPoint.position)) == null);
-                isBehindWall = !isInside && (((environmentTileMap.GetTile(m_GridLayout.WorldToCell(transform.position)) != null) || (underwallTileMap.GetTile(m_GridLayout.WorldToCell(m_SpriteSortPoint.position)) != null) || (entryExitTilemap.GetTile(m_GridLayout.WorldToCell(m_SpriteSortPoint.position)) != null)) && (buildingTileMap.GetTile(m_GridLayout.WorldToCell(m_SpriteSortPoint.position)) == null));
+                isBehindWall = !isInside && (((environmentTileMap.GetTile(m_GridLayout.WorldToCell(transform.position)) != null) || (underwallTileMap.GetTile(m_GridLayout.WorldToCell(m_SpriteSortPoint.position)) != null) || (entryExitTileMap.GetTile(m_GridLayout.WorldToCell(m_SpriteSortPoint.position)) != null)) && (buildingTileMap.GetTile(m_GridLayout.WorldToCell(m_SpriteSortPoint.position)) == null));
                 //Players feet is on a floor tile so we up against the wall from the inside
                 if(isInside){
                     m_SpriteRend.sortingOrder = 2;
                     //We want gameobjects that are not the player to be layered in a similar fashion
                     //What we don't want is AI objects walking behind the walls and triggering the transparency effect.
                     underwallTileMap.color = (gameObject.CompareTag("Player"))?Color.Lerp(new Color(1.0f, 1.0f, 1.0f, (transparancyVal/255.0f)), Color.white, 1.00f):Color.white;
-                    entryExitTilemap.color = (gameObject.CompareTag("Player"))?Color.Lerp(new Color(1.0f, 1.0f, 1.0f, (transparancyVal/255.0f)), Color.white, 1.00f):Color.white;
+                    entryExitTileMap.color = (gameObject.CompareTag("Player"))?Color.Lerp(new Color(1.0f, 1.0f, 1.0f, (transparancyVal/255.0f)), Color.white, 1.00f):Color.white;
                 }                
                 //The p[ayer is outside and behind a wall
                 else if(isBehindWall){
                     m_SpriteRend.sortingOrder = 0;
                     underwallTileMap.color = gameObject.CompareTag("Player")?Color.Lerp(Color.white, new Color(1.0f, 1.0f, 1.0f, transparancyVal), 1.00f):Color.white;
-                    entryExitTilemap.color = gameObject.CompareTag("Player")?Color.Lerp(Color.white, new Color(1.0f, 1.0f, 1.0f, transparancyVal), 1.00f):Color.white;
+                    entryExitTileMap.color = gameObject.CompareTag("Player")?Color.Lerp(Color.white, new Color(1.0f, 1.0f, 1.0f, transparancyVal), 1.00f):Color.white;
                 }
                 //we are standing behind something hanging on the walls
                 else if(ornamentsTileMap.GetTile(m_GridLayout.WorldToCell(m_OrnamentSortPoint.position)) != null)
@@ -64,7 +64,7 @@ namespace Com.ZiomtechStudios.ForgeExchange{
             buildingTileMap = m_GridLayout.gameObject.transform.Find("tilemap_buildings").gameObject.GetComponent<Tilemap>();
             environmentTileMap = m_GridLayout.gameObject.transform.Find("tilemap_environment").gameObject.GetComponent<Tilemap>();
             underwallTileMap = m_GridLayout.gameObject.transform.Find("tilemap_underwall").gameObject.GetComponent<Tilemap>();
-            entryExitTilemap = m_GridLayout.gameObject.transform.Find("tilemap_underwall/tilemap_entry_exit").gameObject.GetComponent<Tilemap>();
+            entryExitTileMap = m_GridLayout.gameObject.transform.Find("tilemap_underwall/tilemap_entry_exit").gameObject.GetComponent<Tilemap>();
             ornamentsTileMap  = m_GridLayout.gameObject.transform.Find("tilemap_underwall/tilemap_ornaments").gameObject.GetComponent<Tilemap>();
             waterTileMap = m_GridLayout.gameObject.transform.Find("tilemap_environment/tilemap_water").gameObject.GetComponent<Tilemap>();
             m_SpriteSortPoint = transform.Find("SpriteSortPos");
@@ -81,6 +81,7 @@ namespace Com.ZiomtechStudios.ForgeExchange{
         void OnTriggerExit2D(Collider2D col){
             SortTargetSprite(col);
             underwallTileMap.color = Color.Lerp(new Color(1.0f, 1.0f, 1.0f, (transparancyVal/255.0f)), Color.white, 1.00f);
+            entryExitTileMap.color = Color.Lerp(new Color(1.0f, 1.0f, 1.0f, (transparancyVal/255.0f)), Color.white, 1.00f);
         }
     }
 }
